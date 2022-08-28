270273467b48d06ed401ab13f5d895f8856392ce
Mohammod Ali <mohammodali651@gmail.com>
2022-08-29 01:48:18 +0600

    todo function added

--- js/todos.js
+++ js/todos.js
@@ -1,4 +1,26 @@
 function loadTodos(){
-    console.log('loadTodos')
+    fetch('https://jsonplaceholder.typicode.com/todos')    
+    .then(res => res.json())
+    .then(data => displayTodos(data))
 }
+
+function displayTodos(todos){
+    // get the container
+    const todoContainer = document.getElementById('todo-container')
+    for(const todo of todos){
+        // create the child element
+        const todoDiv = document.createElement('div')
+        // set inner text or innerHtml
+        todoDiv.innerHTML = `
+            <h3>title: ${todo.title}</h3>
+            <p>User: ${todo.userId}</p>
+            <p>Is complete: ${todo.completed === true ? 'complete' : 'not complete'}</p>
+
+        `
+        //append child
+        todoContainer.appendChild(todoDiv)
+    }
+}
+
+
 loadTodos()
